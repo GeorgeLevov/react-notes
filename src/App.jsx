bb2610fb18d50425f39c784edbd0bfa8ec389b17
George Levov <georgelevov@gmail.com>
2022-08-12 19:09:42 +0300

    Main project completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import Split from "react-split";
+import { nanoid } from "nanoid";
+import Aside from "./components/Aside";
+import Editor from "./components/Editor";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const noteStorage = localStorage.getItem("notes");
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    const [notes, setNotes] = useState(() => JSON.parse(noteStorage) || []);
+
+    const [currentNoteID, setCurrentNoteID] = useState(
+        (notes[0] && notes[0].id) || ""
+    );
+
+    const createNewNote = () => {
+        const newNote = {
+            id: nanoid(),
+            content: "# Note Title . . .",
+        };
+        setNotes((prevNotes) => [newNote, ...prevNotes]);
+        setCurrentNoteID(newNote.id);
+    };
+
+    const updateNoteText = (noteText) => {
+        setNotes((noteArray) => {
+            let newOrder = [];
+            noteArray.map((note) => {
+                note.id === currentNoteID
+                    ? newOrder.unshift({ ...note, content: noteText })
+                    : newOrder.push(note);
+            });
+            return newOrder;
+        });
+    };
+
+    function deleteNote(event, noteId) {
+        event.stopPropagation();
+        setNotes((prevNotes) => {
+            return prevNotes.filter((note) => note.id !== noteId);
+        });
+    }
+
+    useEffect(() => {
+        localStorage.setItem("notes", JSON.stringify(notes));
+    }, [notes]);
+
+    const findCurrentNote = () =>
+        notes.find((note) => note.id === currentNoteID) || notes[0];
+
+    return (
+        <div className="App">
+            {notes.length > 0 ? (
+                <Split
+                    sizes={[30, 70]}
+                    direction="horizontal"
+                    className="split"
+                >
+                    <Aside
+                        notes={notes}
+                        currentNote={findCurrentNote()}
+                        setCurrentNoteId={setCurrentNoteID}
+                        newNote={createNewNote}
+                        deleteNote={deleteNote}
+                    />
+
+                    <Editor
+                        currentNote={findCurrentNote()}
+                        updateNote={updateNoteText}
+                    />
+                </Split>
+            ) : (
+                <div className="fresh-page">
+                    <h1>You have no notes</h1>
+                    <button className="first-note" onClick={createNewNote}>
+                        Create one now
+                    </button>
+                </div>
+            )}
+        </div>
+    );
 }
 
-export default App
+export default App;
