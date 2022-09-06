4e4909aeb29bd2b5cfbe29af1d5bafb07e3c8a37
GeorgeLevov <32199580+GeorgeLevov@users.noreply.github.com>
2022-09-07 02:07:45 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     minify: false,
-    mode: developement,
-    minifySyntax: false
+    minify: false,
+    minifyIdentifiers: true,
+    minifySyntax: true,
 });
